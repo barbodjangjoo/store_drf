6f41cab0d342b4691478bcfeaa685ac88825dd1f
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-11-05 09:23:53 +0330

    add category_list cbv

--- store/urls.py
+++ store/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product_list'),
     path('products/<int:pk>/', views.ProductDetail.as_view()),
-    path('categories/', views.category_list),
+    path('categories/', views.CategoryList.as_view()),
     path('categories/<int:pk>/', views.category_detail, name='category-detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -56,27 +56,22 @@ class ProductDetail(APIView):
         product.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class CategoryList(APIView):
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def product_detail(request, pk):    
-    product = get_object_or_404(
-        Product.objects.select_related('category'),
-        pk=pk
-        )
-    if request.method == 'GET':
-        serializer = ProductSerializer(product, context = {'request': request})
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = ProductSerializer(product, data=request.data)
-        serializer.is_valid()
+    def get(self, request):
+            queryset = Category.objects.annotate(
+            products_count=Count('products')
+            ).all()
+            serializer = CategorySerializer(queryset, many=True)
+            return Response(serializer.data)
+    
+    def post(self, request):
+        serializer = CategorySerializer(data= request.data)
+        serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data)
-    elif request.method == 'DELETE':
-        if product.order_items.count() > 0:
-            return Response({'error': 'Product is in use'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    
 @api_view(['GET', 'POST'])
 def category_list(request):
     if request.method == 'GET':
