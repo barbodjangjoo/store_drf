0dbb9da707313ac78dcfab71e9c55b05f5fb2f74
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-12-22 11:09:15 +0330

    add related name to discounts

--- store/models.py
+++ store/models.py
@@ -27,7 +27,7 @@ class Product(models.Model):
     inventory = models.IntegerField(validators=[MinValueValidator(0)])
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
-    discounts = models.ManyToManyField(Discount, blank=True)
+    discounts = models.ManyToManyField(Discount, blank=True, related_name='discounts')
 
     def __str__(self):
         return self.name
