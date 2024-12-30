b37f1d2ed9c76c5c2ef3c08430fd25b79534fcaa
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-12-30 17:24:32 +0330

    add practice 3 and 4

--- store/urls.py
+++ store/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('practice_one/', views.practice_one, name='show_data'),
     path('practice_two/', views.practice_two,),
+    path('practice_three/', views.practice_three,),
+    path('practice_four/', views.practice_four,),
+    # path('practice_five/', views.practice_five,),
+    # path('practice_six/', views.practice_six,),
+    # path('practice_seven/', views.practice_seven,),
 ]
--- store/views.py
+++ store/views.py
@@ -12,3 +12,11 @@ def practice_one(request):
 def practice_two(request):
     queryset = models.Product.objects.all().filter(inventory=5)
     return render(request, 'store/hello.html', {'products': list(queryset)})
+
+def practice_three(request):
+    queryset = models.Product.objects.filter(name__icontains='site', inventory__gt=3)
+    return render(request, 'store/hello.html', {'products': list(queryset)})
+
+def practice_four(request):
+    queryset = models.Product.objects.filter(name__icontains='site', inventory__gt=3, inventory__lt=10)
+    return render(request, 'store/hello.html', {'products': list(queryset)})
