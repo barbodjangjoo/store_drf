e3849604e431bef64b2049ef82a333769d9a9440
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2024-12-30 23:48:57 +0330

    add more practice

--- store/urls.py
+++ store/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('practice_two/', views.practice_two,),
     path('practice_three/', views.practice_three,),
     path('practice_four/', views.practice_four,),
-    # path('practice_five/', views.practice_five,),
-    # path('practice_six/', views.practice_six,),
+    path('practice_five/', views.practice_five,),
+    path('practice_six/', views.practice_six,),
     # path('practice_seven/', views.practice_seven,),
 ]
--- store/views.py
+++ store/views.py
@@ -20,3 +20,23 @@ def practice_three(request):
 def practice_four(request):
     queryset = models.Product.objects.filter(name__icontains='site', inventory__gt=3, inventory__lt=10)
     return render(request, 'store/hello.html', {'products': list(queryset)})
+
+def practice_five(request):
+    queryset = models.Order.objects.all().filter(status=models.Order.ORDER_STATUS_UNPAID)
+    return render(request, 'store/hello.html', {'products': list(queryset)})
+
+def practice_six(request):
+    queryset = models.Order.objects.filter(id=1)
+    return render(request, 'store/hello.html', {'products': list(queryset)})
+
+def practice_seven(request):
+    queryset = models.OrderItem.objects.filter(order__id=1)
+    return render(request, 'store/hello.html', {'products': list(queryset)})
+
+def practice_eight(request):
+    queryset = models.Product.objects.filter(unit_price__lt=10)
+    return render(request, 'store/hello.html', {'products': list(queryset)})
+
+def practice_nine(request):
+    queryset = models.OrderItem.objects.filter(order__customer__first_name__icontains='john')
+    return render(request, 'store/hello.html', {'products': list(queryset)})
