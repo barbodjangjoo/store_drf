4184725d4925839aedcafa807d8a5949e062f484
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-01-04 17:58:51 +0330

    solve practice number one for aggregate

--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('show_data/', views.show_data, name='show_data'),
-    path('practice_two/', views.practice_two),
-    path('practice_three/', views.practice_three,)
 ]
--- store/views.py
+++ store/views.py
@@ -1,14 +1,11 @@
 from django.shortcuts import render
+from django.db.models import Count, Avg, Max, Min, Sum
 from . import models
 
 def show_data(request):
-    queryset = models.Comment.select_related('product').objects.all() 
-    return render(request, 'store/hello.html',context={'comments':queryset})
-
-def practice_two(request):
-    queryset = models.Product.objects.prefetch_related('comments').all()
-    return render(request, 'store/practice_two.html', context={'products': queryset})
-    
-def practice_three(request):
-    queryset = models.Order.objects.prefetch_related('items__product ').select_related('customer').all()
-    return render(request, 'store/practice_three.html', {'orders': queryset})
+    inventory_10 = models.Product.objects.filter(inventory__gt=10).aggregate(
+        price=Avg('unit_price'),
+        count= Count('id')
+        )
+    print(inventory_10)
+    return render(request, 'store/hello.html',)
