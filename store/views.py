3904d395f8011cbe56b7aafb0630834ef196af5d
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-01-08 02:08:13 +0330

    learn annotations

--- store/views.py
+++ store/views.py
@@ -1,11 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Count, Avg, Max, Min, Sum
+from django.db.models import Count, Avg, Max, Min, Sum, F
 from . import models
 
 def show_data(request):
-    inventory_10 = models.Product.objects.filter(inventory__gt=10).aggregate(
-        price=Avg('unit_price'),
-        count= Count('id')
-        )
-    print(inventory_10)
+    queryset = models.OrderItem.objects.annotate(total_price=F("quantity")* F("unit_price")).all()
+    print(queryset)
     return render(request, 'store/hello.html',)
