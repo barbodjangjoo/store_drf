0ad48d34f37a1e602ec08b61e88004f4e5eccae8
Barbod Jangjoo <barbod.jangjoo96@gmail.com>
2025-01-10 16:34:25 +0330

    learn func

--- store/urls.py
+++ store/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('show_data/', views.show_data, name='show_data'),
+    path('', views.show_data, name='show_data'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
-from django.db.models import Count, Avg, Max, Min, Sum, F
+from django.db.models import Count, Avg, Max, Min, Sum, F, Func, Value
 from . import models
 
 def show_data(request):
-    queryset = models.OrderItem.objects.annotate(total_price=F("quantity")* F("unit_price")).all()
+    queryset = models.Customer.objects.annotate(fullname=Func(F('first_name'), Value(' '), F('last_name'), function='CONCAT'))
     print(queryset)
     return render(request, 'store/hello.html',)
